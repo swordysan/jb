6af4c38e3bf64c50c6927adbd0a3073c1271f256
swordysan <xsword_x@yahoo.co.uk>
2016-09-06 22:08:56 +0100

    Admin command test and tweaks

--- jimbot.js
+++ jimbot.js
@@ -1,18 +1,8 @@
 "use strict";
 var Discord = require("discord.js");
 var jimbot = new Discord.Client();
+var ownerid ="99912330690707456";
 
-//Heroku Error R10 $PORT within 60 seconds of launch error band-aid fix - won't be needed when we find better hosting
-var http = require('http');
-
-http.createServer(function (req, res) {
-
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-res.send('it is running\n');
-
-}).listen(process.env.PORT || 5000);
-//Fix end
 
 let jimin = require('./lists/jimin.json');
 let jimblep = require('./lists/jimblep.json');
@@ -33,20 +23,20 @@ jimbot.on("message", function(message) {
     if(message.content === ("!jimin")) {
 		jimbot.reply(message, jimin[Math.floor(Math.random() * (jimin.length))]);
 	};
-	
+
 	if(message.content.startsWith(prefix + "jimblep")) {
 		jimbot.reply(message, jimblep[Math.floor(Math.random() * (jimblep.length))]);
 	};
-	
+
 	if(message.content.startsWith(prefix + "jiminsta")) {
 		jimbot.reply(message, jiminsta[Math.floor(Math.random() * (jiminsta.length))]);
 	};
-	
+
 	if(message.content.startsWith(prefix + "info")) {
         jimbot.sendMessage(message, "Made out of love for the prettiest girl in the world - in development. Remember to click the link for full res images. ");
 		commandcount--;
     };
-	
+
 	if(message.content.startsWith(prefix + "commands")) {
         jimbot.sendMessage(message, "`!jimin` - Generates a random Jimin.\n\
 `!jimblep` - Gives a random top madam blep.\n\
@@ -59,7 +49,7 @@ jimbot.on("message", function(message) {
 ");
 		commandcount--;
     };
-	
+
 		if(message.content.startsWith(prefix + "roadmap")) {
         jimbot.sendMessage(message, "Features currently being worked on:\n\
 		Performance and Fansign commands for more advanced filtering.\n\
@@ -73,19 +63,28 @@ jimbot.on("message", function(message) {
 		jimbot.sendMessage(message, "Online for **" + msToTime(jimbot.uptime) + "s** and delivered **" + jimmers + "** pretty jimmers. Current pool size 682 images in 28 albums.");
 		commandcount--;
     };
-	
+
 	if(message.content.startsWith(prefix + "status")) {
 		jimbot.sendMessage(message, "Online for **" + msToTime(jimbot.uptime) + "s** and delivered **" + jimmers + "** pretty jimmers. Current pool size 682 images in 28 albums.");
 		commandcount--;
 	};
+
 	if(message.content.startsWith(prefix + "changelog")) {
 		jimbot.sendMessage(message, "**160906** - added changelog and increased nr of !jiminsta from 28 to 99 images (videos will be added soon)\n\
 		");
 		commandcount--;
 	};
+
+	//Testing Adming commands with ownerid
+	if(message.author.id === ownerid){
+		if(message.content.startsWith(prefix + "hello")) {
+			jimbot.reply(message, "Hello pretty!");
+			commandcount--;
+		}
+	};
 });
 
-
+//Uptime and status formatting
 function msToTime(s) {
 
   function addZ(n) {
@@ -101,6 +100,13 @@ function msToTime(s) {
 
   return addZ(hrs) + ':' + addZ(mins) + ':' + addZ(secs);
 }
+
+//Ready?
+jimbot.on("ready", function(){
+    console.log("Bot Name : " + jimbot.user.username);
+    console.log("Bot ID : " + jimbot.user.id);
+});
+
 
 jimbot.on('error', e => { console.error(e); });
 jimbot.on('warn', e => { console.warn(e); });
